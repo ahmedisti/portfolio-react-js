e8a3bd0a52547a3d6b490357cf56b25881aad67a
ahmedisti <mdistiaqah@gmail.com>
2022-12-25 22:02:49 +0600

    header section start

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import './Header.css'
+import './Header.css';
+import CTA from './CTA';
+import ME from '../../assets/me.png';
 
 const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+        <header>
+            <div className="container header__container">
+                <h5>Assalamu-alaikum I'm</h5>
+                <h1>Istiaq Ahmed</h1>
+                <h5 className="text-light">Frontend Developer</h5>
+                <CTA></CTA>
+
+                <div className="me">
+                    <img src={ME}alt="me" srcset="" />
+                </div>
+                <a href="#contact" className='scroll__down'>Scroll Down</a>
+            </div>
+        </header>
     );
 };
 
