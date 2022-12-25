25ca5977766bf3cc4381196126c1d348259db62a
ahmedisti <mdistiaqah@gmail.com>
2022-12-25 18:09:08 +0600

    project start

---
+++ src/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Testimonial.css'
+
+const Testimonials = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Testimonials;
